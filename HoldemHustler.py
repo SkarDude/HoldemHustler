96e2f55bdf582ef74dc82e20439d8dd2576c5932
Alex Nagel <Alex Nagel>
2017-03-26 15:19:11 -0400

    Dealing hole cards and community cards

--- HoldemHustler.py
+++ HoldemHustler.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import random
 
 # from numpy import *
 # import scipy as sp
@@ -15,6 +16,23 @@ d = range(2,15)
 c = range(2,15)
 h = range(2,15)
 s = range(2,15)
+Diamonds = 1
+Clubs = 2
+Hearts = 3
+Spades = 4
+deck = []
+
+# General Variables
+players = raw_input("Players: ")
+players = int(players) - 1
+players_hands = []
+players_seven = []
+my_hand = []
+burn_cards = []
+burn = 3
+community_cards = []
+community = 5
+my_seven = []
 
 # capture cards
 cards = raw_input("My Cards: ")
@@ -23,48 +41,84 @@ card1 = list(cards[0])
 card2 = list(cards[1])
 
 # card cleanup
-if card1[0] == 't':
+if str(card1[0]).lower() == 't':
 	card1[0] = 10
-if card1[0] == 'j':
+if str(card1[0]).lower() == 'j':
 	card1[0] = 11
-if card1[0] == 'q':
+if str(card1[0]).lower() == 'q':
 	card1[0] = 12
-if card1[0] == 'k':
+if str(card1[0]).lower() == 'k':
 	card1[0] = 13 
-if card1[0] == 'a':
+if str(card1[0]).lower() == 'a':
 	card1[0] = 14
-	card1.append(1) 
 
-if card2[0] == 't':
+if str(card2[0]).lower() == 't':
 	card2[0] = 10
-if card2[0] == 'j':
+if str(card2[0]).lower() == 'j':
 	card2[0] = 11
-if card2[0] == 'q':
+if str(card2[0]).lower() == 'q':
 	card2[0] = 12
-if card2[0] == 'k':
+if str(card2[0]).lower() == 'k':
 	card2[0] = 13 
-if card2[0] == 'a':
+if str(card2[0]).lower() == 'a':
 	card2[0] = 14
-	card2.append(1)
 
-# remove cards from deck
-if card1[1] == 'd':
+# remove cards from deck and change to int
+if str(card1[1]).lower() == 'd':
 	d.remove(int(card1[0]))
-if card1[1] == 'c':
+	card1[1] = Diamonds
+if str(card1[1]).lower() == 'c':
 	c.remove(int(card1[0]))
-if card1[1] == 's':
+	card1[1] = Clubs
+if str(card1[1]).lower() == 's':
 	s.remove(int(card1[0]))
-if card1[1] == 'h':
+	card1[1] = Spades
+if str(card1[1]).lower() == 'h':
 	h.remove(int(card1[0]))
+	card1[1] = Hearts
 
-if card2[1] == 'd':
+if str(card2[1]).lower() == 'd':
 	d.remove(int(card2[0]))
-if card2[1] == 'c':
+	card2[1] = Diamonds
+if str(card2[1]).lower() == 'c':
 	c.remove(int(card2[0]))
-if card2[1] == 's':
+	card2[1] = Clubs
+if str(card2[1]).lower() == 's':
 	s.remove(int(card2[0]))
-if card2[1] == 'h':
+	card2[1] = Spades
+if str(card2[1]).lower() == 'h':
 	h.remove(int(card2[0]))
+	card2[1] = Hearts
+
+card1[0] = int(card1[0])
+card2[0] = int(card2[0])
+my_hand.append([card1, card2])
 
 
+for card in d:
+	deck.append([card, Diamonds])
+for card in c:
+	deck.append([card, Clubs])
+for card in h:
+	deck.append([card, Hearts])
+for card in s:
+	deck.append([card, Spades])
 
+# generate random hands every time
+random.shuffle(deck)
+
+for card in range(burn):
+	burn_cards.append(deck[card])
+	deck.remove(deck[card])
+
+for card in range(community):
+	community_cards.append(deck[card])
+	deck.remove(deck[card])
+
+for player in range(players):
+	players_hands.append([deck[player], deck[player+1]])
+	deck.remove(deck[player])
+	deck.remove(deck[player+1])
+	players_seven.append(players_hands[player] + community_cards)
+
+my_seven = my_hand[0] + community_cards
